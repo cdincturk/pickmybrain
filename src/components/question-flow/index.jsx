8ca17d1f711bc0de525faefedf219d58c41d9fe5
Cenk Dincturk <dincurkcenk@gmail.com>
2023-01-20 01:07:38 +0300

    Add question component

---
+++ src/components/question-flow/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NewQuestion } from "../new-question";
+
+function QuestionFlow() {
+  return (
+    <div>
+      <NewQuestion />
+    </div>
+  );
+}
+
+export default QuestionFlow;
